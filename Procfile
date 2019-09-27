b5edc5c218d4e4182c5ea85d8c72613589b7699f
Fatih POLATLI <fatihpolatli@gmail.com>
2019-09-27 13:00:06 +0300

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: nodemon transpiled/index.js
+web: npm start
--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
 
 
 app.use("/api", apiRouter);
